43d1d93525b705b2f81f9a29fe86928351a947bc
alainagarcia <alaina_garcia@ucne.edu.do>
2025-07-06 11:09:41 -0400

    Agregando la base de la carpeta di

---
+++ app/src/main/java/edu/ucne/recrearte/di/AppModule.kt
@@ -0,0 +1,10 @@
+package edu.ucne.recrearte.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+}
