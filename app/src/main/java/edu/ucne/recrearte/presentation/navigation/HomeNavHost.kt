b9c90acd91bd6d4e85ef59d6a703641090fb9c7b
alainagarcia <alaina_garcia@ucne.edu.do>
2025-07-06 11:10:06 -0400

    Agregando la base de la carpeta presentation

---
+++ app/src/main/java/edu/ucne/recrearte/presentation/navigation/HomeNavHost.kt
@@ -0,0 +1,9 @@
+package edu.ucne.recrearte.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun HomeNavHost(
+    navHostController: NavHostController
+){}
---
+++ app/src/main/java/edu/ucne/recrearte/presentation/navigation/Screen.kt
@@ -0,0 +1,3 @@
+package edu.ucne.recrearte.presentation.navigation
+
+sealed class Screen {}
