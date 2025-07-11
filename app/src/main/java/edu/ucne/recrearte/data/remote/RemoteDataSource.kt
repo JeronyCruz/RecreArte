1a75e45cad777133c9bf3b4a0193ed54fdba7e95
alainagarcia <alaina_garcia@ucne.edu.do>
2025-07-11 17:58:56 -0400

    Endpoint agregados para PaymentMethod

--- app/src/main/java/edu/ucne/recrearte/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/recrearte/data/remote/RemoteDataSource.kt
@@ -1,7 +1,16 @@
 package edu.ucne.recrearte.data.remote
 
+import edu.ucne.recrearte.data.remote.dto.PaymentMethodDto
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
     private val recreArteingApi: RecreArteingApi
-){}
+){
+    //PaymentMethod
+    suspend fun getPaymentMethod() = recreArteingApi.getAllPaymentMethod()
+    suspend fun updatePaymentMethod(id: Int, paymentMethodDto: PaymentMethodDto) =
+        recreArteingApi.updatePaymentMethod(id, paymentMethodDto)
+    suspend fun getById(id: Int) = recreArteingApi.getByIdPaymentMethod(id)
+    suspend fun createPaymentMethod(paymentMethodDto: PaymentMethodDto) = recreArteingApi.createPaymentMethod(paymentMethodDto)
+    suspend fun deletePaymentMethod(id: Int) = recreArteingApi.deletePaymentMethod(id)
+}
