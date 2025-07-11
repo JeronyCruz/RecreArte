cbfcd9173ddf4e43358daf9e4b7c0cb4b1b79f01
alainagarcia <alaina_garcia@ucne.edu.do>
2025-07-11 17:59:15 -0400

    ApiModule realizado

--- app/src/main/java/edu/ucne/recrearte/di/ApiModule.kt
+++ app/src/main/java/edu/ucne/recrearte/di/ApiModule.kt
@@ -1,10 +1,35 @@
 package edu.ucne.recrearte.di
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import edu.ucne.recrearte.data.remote.RecreArteingApi
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object ApiModule {
+    const val BASE_URL = "https://recreartev1.azurewebsites.net/"
+
+    @Provides
+    @Singleton
+    fun providesMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .add(DateAdapter()).build()
+
+    @Provides
+    @Singleton
+    fun providesRecreArteApi(moshi: Moshi): RecreArteingApi{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build().create(RecreArteingApi::class.java)
+    }
 }
