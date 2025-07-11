bb3bb3e3afcf8d9506c80c763e2a6a23df9fe35d
alainagarcia <alaina_garcia@ucne.edu.do>
2025-07-11 18:01:01 -0400

    MainActivity actualizado

--- app/src/main/java/edu/ucne/recrearte/MainActivity.kt
+++ app/src/main/java/edu/ucne/recrearte/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.recrearte.presentation.navigation.HomeNavHost
 import edu.ucne.recrearte.ui.theme.RecreArteTheme
 
 @AndroidEntryPoint
@@ -20,8 +22,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val nav = rememberNavController()
             RecreArteTheme {
-
+                HomeNavHost(
+                    navHostController = nav
+                )
             }
         }
     }
