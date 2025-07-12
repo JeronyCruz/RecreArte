99bf1667305d3644614899668ec317236840ce8d
alainagarcia <alaina_garcia@ucne.edu.do>
2025-07-12 19:01:07 -0400

    Feat: Added funtion to search and correction of edit funtion

--- app/src/main/java/edu/ucne/recrearte/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/recrearte/data/remote/RemoteDataSource.kt
@@ -2,8 +2,9 @@ package edu.ucne.recrearte.data.remote
 
 import edu.ucne.recrearte.data.remote.dto.LoginRequestDto
 import edu.ucne.recrearte.data.remote.dto.LoginResponseDto
-import edu.ucne.recrearte.data.remote.dto.PaymentMethodDto
+import edu.ucne.recrearte.data.remote.dto.PaymentMethodsDto
 import edu.ucne.recrearte.data.remote.dto.TechniquesDto
+import retrofit2.HttpException
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
@@ -16,16 +17,40 @@ class RemoteDataSource @Inject constructor(
 
     //PaymentMethod
     suspend fun getPaymentMethod() = recreArteingApi.getAllPaymentMethod()
-    suspend fun updatePaymentMethod(id: Int, paymentMethodsDto: PaymentMethodsDto) =
-        recreArteingApi.updatePaymentMethod(id, paymentMethodsDto)
+    suspend fun getPaymentMethodById(id: Int): PaymentMethodsDto {
+        val response = recreArteingApi.getByIdPaymentMethod(id)
+        if (response.isSuccessful) {
+            return response.body() ?: throw Exception("Payment Method not found")
+        } else {
+            throw HttpException(response)
+        }
+    }
+    suspend fun updatePaymentMethod(id: Int, paymentMethodDto: PaymentMethodsDto) {
+        val response = recreArteingApi.updatePaymentMethod(id, paymentMethodDto)
+        if (!response.isSuccessful) {
+            throw HttpException(response)
+        }
+    }
     suspend fun getById(id: Int) = recreArteingApi.getByIdPaymentMethod(id)
     suspend fun createPaymentMethod(paymentMethodsDto: PaymentMethodsDto) = recreArteingApi.createPaymentMethod(paymentMethodsDto)
     suspend fun deletePaymentMethod(id: Int) = recreArteingApi.deletePaymentMethod(id)
 
     //Techniques
     suspend fun getTechniques() = recreArteingApi.getAllTechniques()
-    suspend fun updateTechnique(id: Int, techniqueDto: TechniquesDto) =
-        recreArteingApi.updateTechnique(id, techniqueDto)
+    suspend fun getTechniqueById(id: Int): TechniquesDto {
+        val response = recreArteingApi.getByIdTecnique(id)
+        if (response.isSuccessful) {
+            return response.body() ?: throw Exception("Technique not found")
+        } else {
+            throw HttpException(response)
+        }
+    }
+    suspend fun updateTechnique(id: Int, techniqueDto: TechniquesDto) {
+        val response = recreArteingApi.updateTechnique(id, techniqueDto)
+        if (!response.isSuccessful) {
+            throw HttpException(response)
+        }
+    }
     suspend fun createTechnique(techniqueDto: TechniquesDto) = recreArteingApi.createTechnique(techniqueDto)
     suspend fun deleteTechnique(id: Int) = recreArteingApi.deleteTechnique(id)
 }
