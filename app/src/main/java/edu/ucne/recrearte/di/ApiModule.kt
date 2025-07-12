2b4dd9d4698ccbae79ec73d40dc09ab120b9bfe2
JeronyCruz <jerony_cruz@ucne.edu.do>
2025-07-12 16:00:10 -0400

    Fix: ProvidesRecreArteApi

--- app/src/main/java/edu/ucne/recrearte/di/ApiModule.kt
+++ app/src/main/java/edu/ucne/recrearte/di/ApiModule.kt
@@ -1,15 +1,22 @@
 package edu.ucne.recrearte.di
 
+import android.content.Context
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import edu.ucne.recrearte.data.remote.RecreArteingApi
+import edu.ucne.recrearte.data.remote.RemoteDataSource
+import edu.ucne.recrearte.util.TokenManager
+import edu.ucne.recrearte.data.repository.AuthInterceptor
+import edu.ucne.recrearte.util.TokenManagement
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.create
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -26,10 +33,55 @@ object ApiModule {
 
     @Provides
     @Singleton
-    fun providesRecreArteApi(moshi: Moshi): RecreArteingApi{
+    fun provideAuthInterceptor(tokenManager: TokenManager): AuthInterceptor {
+        return AuthInterceptor(tokenManager)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        authInterceptor: AuthInterceptor,
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesRecreArteApi(
+        moshi: Moshi,
+        okHttpClient: OkHttpClient
+    ): RecreArteingApi {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient) // AQUI AÑADES EL CLIENTE
             .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .build().create(RecreArteingApi::class.java)
+            .build()
+            .create(RecreArteingApi::class.java)
+    }
+
+
+
+    @Provides
+    @Singleton
+    fun provideRemoteDataSource(api: RecreArteingApi): RemoteDataSource {
+        return RemoteDataSource(api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideTokenManager(@ApplicationContext context: Context): TokenManager {
+        return TokenManagement(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
     }
 }
