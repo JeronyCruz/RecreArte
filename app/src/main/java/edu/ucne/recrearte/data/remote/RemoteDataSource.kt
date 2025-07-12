d6e76da46cdb3e9857ad2b82a3d26f392bdf735c
JeronyCruz <jerony_cruz@ucne.edu.do>
2025-07-12 16:23:59 -0400

    Feat: Add loginUser.

--- app/src/main/java/edu/ucne/recrearte/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/recrearte/data/remote/RemoteDataSource.kt
@@ -1,11 +1,18 @@
 package edu.ucne.recrearte.data.remote
 
+import edu.ucne.recrearte.data.remote.dto.LoginRequestDto
+import edu.ucne.recrearte.data.remote.dto.LoginResponseDto
 import edu.ucne.recrearte.data.remote.dto.PaymentMethodDto
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
     private val recreArteingApi: RecreArteingApi
 ){
+    // Login
+    suspend fun loginUser(loginRequest: LoginRequestDto): LoginResponseDto {
+        return recreArteingApi.loginUser(loginRequest)
+    }
+
     //PaymentMethod
     suspend fun getPaymentMethod() = recreArteingApi.getAllPaymentMethod()
     suspend fun updatePaymentMethod(id: Int, paymentMethodDto: PaymentMethodDto) =
