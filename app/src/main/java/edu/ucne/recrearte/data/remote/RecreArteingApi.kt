7bed61053ac3ad35defbed5d0813fe2ce597c5e2
alainagarcia <alaina_garcia@ucne.edu.do>
2025-07-12 00:24:10 -0400

    Endpoints de technique agregados

--- app/src/main/java/edu/ucne/recrearte/data/remote/RecreArteingApi.kt
+++ app/src/main/java/edu/ucne/recrearte/data/remote/RecreArteingApi.kt
@@ -1,6 +1,7 @@
 package edu.ucne.recrearte.data.remote
 
-import edu.ucne.recrearte.data.remote.dto.PaymentMethodDto
+import edu.ucne.recrearte.data.remote.dto.PaymentMethodsDto
+import edu.ucne.recrearte.data.remote.dto.TechniquesDto
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
@@ -11,23 +12,32 @@ import retrofit2.http.Path
 
 interface RecreArteingApi {
 
-    //PaymentMethod
+    //PaymentMethods
     @GET("api/PaymentMethods")
-    suspend fun getAllPaymentMethod(): List<PaymentMethodDto>
+    suspend fun getAllPaymentMethod(): List<PaymentMethodsDto>
 
     @GET("api/PaymentMethods/{id}")
-    suspend fun getByIdPaymentMethod(@Path("id") id: Int): PaymentMethodDto
-
+    suspend fun getByIdPaymentMethod(@Path("id") id: Int): PaymentMethodsDto
     @POST("api/PaymentMethods")
-    suspend fun createPaymentMethod(@Body paymentMethodDto:  PaymentMethodDto): PaymentMethodDto
-
+    suspend fun createPaymentMethod(@Body paymentMethodsDto:  PaymentMethodsDto): PaymentMethodsDto
     @PUT("api/PaymentMethods/{id}")
     suspend fun updatePaymentMethod(
         @Path("id") id: Int,
-        @Body paymentMethodDto: PaymentMethodDto
-    ): PaymentMethodDto
-
+        @Body paymentMethodsDto: PaymentMethodsDto
+    ): PaymentMethodsDto
     @DELETE("api/PaymentMethods/{id}")
     suspend fun deletePaymentMethod(@Path("id") id: Int): Response<Unit>
 
+    // Techniques
+    @GET("api/Techniques")
+    suspend fun getAllTechniques(): List<TechniquesDto>
+    @POST("api/Techniques")
+    suspend fun createTechnique(@Body techniqueDto:  TechniquesDto): TechniquesDto
+    @PUT("api/Techniques/{id}")
+    suspend fun updateTechnique(
+        @Path("id") id: Int,
+        @Body techniqueDto: TechniquesDto
+    ): TechniquesDto
+    @DELETE("api/Techniques/{id}")
+    suspend fun deleteTechnique(@Path("id") id: Int): Response<Unit>
 }
--- app/src/main/java/edu/ucne/recrearte/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/recrearte/data/remote/RemoteDataSource.kt
@@ -1,6 +1,7 @@
 package edu.ucne.recrearte.data.remote
 
-import edu.ucne.recrearte.data.remote.dto.PaymentMethodDto
+import edu.ucne.recrearte.data.remote.dto.PaymentMethodsDto
+import edu.ucne.recrearte.data.remote.dto.TechniquesDto
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
@@ -8,9 +9,16 @@ class RemoteDataSource @Inject constructor(
 ){
     //PaymentMethod
     suspend fun getPaymentMethod() = recreArteingApi.getAllPaymentMethod()
-    suspend fun updatePaymentMethod(id: Int, paymentMethodDto: PaymentMethodDto) =
-        recreArteingApi.updatePaymentMethod(id, paymentMethodDto)
+    suspend fun updatePaymentMethod(id: Int, paymentMethodsDto: PaymentMethodsDto) =
+        recreArteingApi.updatePaymentMethod(id, paymentMethodsDto)
     suspend fun getById(id: Int) = recreArteingApi.getByIdPaymentMethod(id)
-    suspend fun createPaymentMethod(paymentMethodDto: PaymentMethodDto) = recreArteingApi.createPaymentMethod(paymentMethodDto)
+    suspend fun createPaymentMethod(paymentMethodsDto: PaymentMethodsDto) = recreArteingApi.createPaymentMethod(paymentMethodsDto)
     suspend fun deletePaymentMethod(id: Int) = recreArteingApi.deletePaymentMethod(id)
+
+    //Techniques
+    suspend fun getTechniques() = recreArteingApi.getAllTechniques()
+    suspend fun updateTechnique(id: Int, techniqueDto: TechniquesDto) =
+        recreArteingApi.updateTechnique(id, techniqueDto)
+    suspend fun createTechnique(techniqueDto: TechniquesDto) = recreArteingApi.createTechnique(techniqueDto)
+    suspend fun deleteTechnique(id: Int) = recreArteingApi.deleteTechnique(id)
 }
