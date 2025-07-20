847697bde6c241a1f034f90988638803203cb7f2
JeronyCruz <jerony_cruz@ucne.edu.do>
2025-07-20 02:18:06 -0400

    Fix: Add Corrections in Attributes.

--- app/src/main/java/edu/ucne/recrearte/data/remote/dto/ArtistsDto.kt
+++ app/src/main/java/edu/ucne/recrearte/data/remote/dto/ArtistsDto.kt
@@ -4,17 +4,17 @@ import java.util.Date
 
 data class ArtistsDto (
     val artistId : Int?,
-    val artStyle : String,
-    val socialMediaLinks : String,
-    val firstName : String,
-    val lastName : String,
-    val email : String,
-    val password : String,
-    val userName : String,
-    val phoneNumber : String,
-    val documentNumber : String,
+    val artStyle : String?,
+    val socialMediaLinks : String?,
+    val firstName : String?,
+    val lastName : String?,
+    val email : String?,
+    val password : String?,
+    val userName : String?,
+    val phoneNumber : String?,
+    val documentNumber : String?,
     val updateAt : Date,
     val roleId : Int,
     val description : String?,
-    val token: String
+    val token: String?
 )
--- app/src/main/java/edu/ucne/recrearte/data/remote/dto/CustomersDto.kt
+++ app/src/main/java/edu/ucne/recrearte/data/remote/dto/CustomersDto.kt
@@ -4,16 +4,16 @@ import java.util.Date
 
 data class CustomersDto(
     val customerId : Int?,
-    val address : String,
-    val firstName : String,
-    val lastName : String,
-    val email : String,
-    val password : String,
-    val userName : String,
-    val phoneNumber : String,
-    val documentNumber : String,
+    val address : String?,
+    val firstName : String?,
+    val lastName : String?,
+    val email : String?,
+    val password : String?,
+    val userName : String?,
+    val phoneNumber : String?,
+    val documentNumber : String?,
     val updateAt : Date,
     val roleId : Int,
     val description : String?,
-    val token: String
+    val token: String?
 )
