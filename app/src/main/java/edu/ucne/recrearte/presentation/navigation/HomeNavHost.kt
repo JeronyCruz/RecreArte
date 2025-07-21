3bd0ac77af4ceec50742bd69516a1dc2459e7ec4
alainagarcia <alaina_garcia@ucne.edu.do>
2025-07-21 17:30:39 -0400

    Feat: completed shopping cart navigation

--- app/src/main/java/edu/ucne/recrearte/presentation/navigation/HomeNavHost.kt
+++ app/src/main/java/edu/ucne/recrearte/presentation/navigation/HomeNavHost.kt
@@ -18,6 +18,7 @@ import edu.ucne.recrearte.presentation.login.LoginScreen
 import edu.ucne.recrearte.presentation.paymentMethods.PaymentMethodListScreen
 import edu.ucne.recrearte.presentation.paymentMethods.PaymentMethodScreen
 import edu.ucne.recrearte.presentation.profile.ProfileScreen
+import edu.ucne.recrearte.presentation.shoppingCarts.ShoppingCartScreen
 import edu.ucne.recrearte.presentation.signUp.SignUpScreen
 import edu.ucne.recrearte.presentation.techniques.TechniqueListScreen
 import edu.ucne.recrearte.presentation.techniques.TechniqueScreen
@@ -169,7 +170,12 @@ fun HomeNavHost(
         }
 
         composable<Screen.CartScreen> {
-            // Tu pantalla de carrito aquí
+            ShoppingCartScreen(
+                navController = navHostController,
+                onWorkClick = { workId ->
+                    navHostController.navigate(Screen.WorkDetails(workId))
+                }
+            )
         }
 
         composable<Screen.ProfileScreen> {
