bc62b62c088ef87bb0db39fdae7ccb24da616353
alainagarcia <alaina_garcia@ucne.edu.do>
2025-07-23 01:20:55 -0400

    Feat: function checkout added

--- app/src/main/java/edu/ucne/recrearte/data/repository/ShoppingCartRepository.kt
+++ app/src/main/java/edu/ucne/recrearte/data/repository/ShoppingCartRepository.kt
@@ -1,6 +1,7 @@
 package edu.ucne.recrearte.data.repository
 
 import edu.ucne.recrearte.data.remote.RemoteDataSource
+import edu.ucne.recrearte.data.remote.dto.BillsDto
 import edu.ucne.recrearte.data.remote.dto.ShoppingCartsDto
 import javax.inject.Inject
 
@@ -14,4 +15,5 @@ class ShoppingCartRepository @Inject constructor(
     suspend fun addToCart(customerId: Int, workId: Int) = remoteDataSource.addToCart(customerId, workId)
     suspend fun removeFromCart(itemId: Int) = remoteDataSource.removeFromCart(itemId)
     suspend fun clearCart(customerId: Int) = remoteDataSource.clearCart(customerId)
+    suspend fun checkout(customerId: Int): BillsDto = remoteDataSource.checkout(customerId)
 }
