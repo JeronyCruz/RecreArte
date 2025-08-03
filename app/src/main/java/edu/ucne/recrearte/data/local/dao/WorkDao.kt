55b3cc9bf87c4775d09aa2edcaeb4a042b3a73d7
alainagarcia <alaina_garcia@ucne.edu.do>
2025-08-02 20:56:48 -0400

    Feat: add funtion save one

--- app/src/main/java/edu/ucne/recrearte/data/local/dao/WorkDao.kt
+++ app/src/main/java/edu/ucne/recrearte/data/local/dao/WorkDao.kt
@@ -10,6 +10,8 @@ import edu.ucne.recrearte.data.local.entities.WorksEntity
 interface WorkDao{
     @Upsert()
     suspend fun save(works: List<WorksEntity>)
+    @Upsert()
+    suspend fun saveOne(works: WorksEntity)
     @Query(
         """
         SELECT * 
@@ -23,4 +25,18 @@ interface WorkDao{
     suspend fun delete(work: WorksEntity)
     @Query("SELECT * FROM Works")
     suspend fun getAll(): List<WorksEntity>
+    @Query("SELECT * FROM Works WHERE artistId=:artistId")
+    suspend fun getByArtist(artistId: Int): List<WorksEntity>
+    @Query("SELECT * FROM Works WHERE techniqueId=:techniqueId")
+    suspend fun getByTechnique(techniqueId: Int): List<WorksEntity>
+//    @Query("""
+//        SELECT w.*, COUNT(l.workId) as likeCount
+//        FROM Works w
+//        LEFT JOIN Likes l ON w.workId = l.workId
+//        WHERE w.statusId = 1
+//        GROUP BY w.workId
+//        ORDER BY likeCount DESC
+//        LIMIT 5
+//    """)
+//    suspend fun getTop5(): List<WorksEntity>
 }
