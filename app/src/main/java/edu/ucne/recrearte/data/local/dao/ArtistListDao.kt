9cdea613d3a724bb7a91a7b5ea5cbe225ac8c938
alainagarcia <alaina_garcia@ucne.edu.do>
2025-08-02 22:37:28 -0400

    Feat: DAO done

---
+++ app/src/main/java/edu/ucne/recrearte/data/local/dao/ArtistListDao.kt
@@ -0,0 +1,30 @@
+package edu.ucne.recrearte.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.recrearte.data.local.entities.ArtistsListEntity
+import edu.ucne.recrearte.data.remote.dto.ArtistListDto
+
+
+@Dao
+interface ArtistListDao {
+    @Upsert()
+    suspend fun save(artists: List<ArtistsListEntity>)
+    @Upsert()
+    suspend fun saveOne(artists: ArtistsListEntity)
+    @Query(
+        """
+        SELECT * 
+        FROM ArtistsList 
+        WHERE artistId=:id  
+        LIMIT 1
+        """
+    )
+    suspend fun find(id: Int): ArtistsListEntity?
+    @Delete
+    suspend fun delete(artist: ArtistsListEntity)
+    @Query("SELECT * FROM ArtistsList")
+    suspend fun getAll(): List<ArtistsListEntity>
+}
