3d77406ce83f00f6df3a96682d6a0e81fc15925d
alainagarcia <alaina_garcia@ucne.edu.do>
2025-08-04 10:41:54 -0400

    Fix: filtered by the status of the added work

--- app/src/main/java/edu/ucne/recrearte/data/local/dao/WorkDao.kt
+++ app/src/main/java/edu/ucne/recrearte/data/local/dao/WorkDao.kt
@@ -16,18 +16,18 @@ interface WorkDao{
         """
         SELECT * 
         FROM Works 
-        WHERE workId=:id  
+        WHERE workId=:id  AND statusId = 1 
         LIMIT 1
         """
     )
     suspend fun find(id: Int): WorksEntity?
     @Delete
     suspend fun delete(work: WorksEntity)
-    @Query("SELECT * FROM Works")
+    @Query("SELECT * FROM Works WHERE statusId = 1")
     suspend fun getAll(): List<WorksEntity>
-    @Query("SELECT * FROM Works WHERE artistId=:artistId")
+    @Query("SELECT * FROM Works WHERE artistId=:artistId AND statusId = 1")
     suspend fun getByArtist(artistId: Int): List<WorksEntity>
-    @Query("SELECT * FROM Works WHERE techniqueId=:techniqueId")
+    @Query("SELECT * FROM Works WHERE techniqueId=:techniqueId AND statusId = 1")
     suspend fun getByTechnique(techniqueId: Int): List<WorksEntity>
     @Query("""
         SELECT w.*
